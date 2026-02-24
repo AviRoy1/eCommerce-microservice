5e0e77057e0dd2b3e984a210416034d27e8cd623
AviRoy1 <avijitroy2944@gmail.com>
2026-02-24 22:13:01 +0530

    added notification service

---
+++ Notification/src/main/java/com/example/Notification/OrderEventConsumer.java
@@ -0,0 +1,15 @@
+package com.example.Notification;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+public class OrderEventConsumer {
+    @RabbitListener(queues = "${rabbitmq.queue.name}")
+    public  void handleOrderEvent(Map<String, Object> orderEvent) {
+        System.out.println("Received order event: " + orderEvent);
+    }
+}
